b98723a1a02a10f67aaa8593fa30de9bb95088c7
TauaFrade <124211685+TauaFrade@users.noreply.github.com>
2023-12-02 19:26:05 -0300

    Update Services.py

---
+++ Services.py
@@ -0,0 +1,22 @@
+#PROJETO FILA DE CHEGADAS E SAIDAS DE UM AEROPORTO
+
+# NOTAS
+# 0.1: INPLEMENTAÇÃO BASICA DE NO E LISTA DUPLAMENTE ENCADEADA
+
+class voo:
+    def __init__(self, horario, destino, chegando, codigo):
+        #Informações do voo
+        self.horario = horario
+        self.status = 'Previto'
+        self.destino = destino
+        self.chegando = chegando
+        self.codigo = codigo
+        #Ponteiros
+        self.prox = None
+        self.ulti = None
+
+class fila_de_voos:
+    def __init__(self):
+        self.cabeca = None
+        self.cauda = None
+        self.tamanho = 0
