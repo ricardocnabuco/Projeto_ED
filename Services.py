3ae3fde8f3a1cd04b028be5a42c18e3aaa20aeb8
Ricardo de Carvalho Nabuco <ricardocnabuco@gmail.com>
2023-12-02 23:40:37 -0300

    Update Services.py

--- Services.py
+++ Services.py
@@ -8,7 +8,7 @@ class voo:
         #Informações do voo
         self.horario = horario
         self.status = 'Previto'
-        self.destino_origem = destino_origem
+        self.destino = destino
         self.codigo = codigo
         self.portao = portao
         #Ponteiros
@@ -68,38 +68,3 @@ class fila_de_voos:
         self.cauda.prox = novo_voo
         novo_voo.anterior = self.cauda
         self.cauda = novo_voo
-
-
-    #Busca
-    # Como dois voos podem compartilhar o mesmo horario e essa variavel é a forma que eles são ordenados se faz necessario um
-    # segundo parametro para a busca
-    def busca_binaria(self, codigo, horario, atual= -1, passos=-1):
-        aux_horas, aux_minutos = horario.split(':')
-        aux_horas = int(aux_horas)
-        aux_minutos = int(aux_minutos)
-        h_aux = (aux_horas*60) + aux_minutos
-        if atual == -1 and passos == -1:
-            atual = self.cabeca
-            passos = self.tamanho // 2
-        elif atual == None:
-            print('Voo não encontrado, verifique se não houve nenhum erro de digitação')
-            return
-        elif passos == 0:
-            passos += 1
-        for _ in range(passos):
-            atual = atual.prox
-        if atual.mod_horario == h_aux:
-            if atual.codigo == codigo: #Verifica se o voo encontrado não é outro com o mesmo horario
-                print(f'Voo {codigo} encontrado.')
-                print(f'Status: {atual.status}')
-                print(f'Horario: {atual.horario}')
-                print(f'Portão: {atual.portao}')
-                print(f'destino/origem: {atual.destino_origem}')
-                return
-            else: # Caso seja verifica os proximos voos, pois pela forma de ordenação é onde estarão
-                self.busca_binaria(codigo, horario, atual, 1)
-        elif atual.mod_horario >  h_aux:
-            self.busca_binaria(codigo, horario, self.cabeca, passos//2)
-        else:
-            self.busca_binaria(codigo, horario, atual, passos//2)
-
