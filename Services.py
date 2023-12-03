07644670f24dc785ed9b192438f7469a9fb398bb
Ricardo de Carvalho Nabuco <ricardocnabuco@gmail.com>
2023-12-03 00:42:35 -0300

    Update Services.py

--- Services.py
+++ Services.py
@@ -105,31 +105,31 @@ class fila_de_voos:
 
 # Travessia
 
-def travessia(self):
-    if self.tamanho == 0:
-        print('Fila vazia! Não há voos para serem percorridos!')
-        return
-    # Imprima o cabeçalho da tabela das PARTIDAS 
-    print("Tabela de PARTIDAS")
-    print(f'{"Horário":<10} {"Código":<10} {"Portão":<10} {"Destino":<15} {"Status":<10}')
-    print("=" * 60)
-    atual = self.cabeca
-    # Imprimir informações de cada voo por linha da tabela das PARTIDAS
-    while atual:
-        if not atual.chegando: 
-            print(f'{atual.horario:<10} {atual.codigo:<10} {atual.portao:<10} {atual.destino:<15} {atual.status:<10}')
-        atual = atual.prox
-    print("\n" + "=" * 60)
-    # Imprimir o cabeçalho da tabela das CHEGADAS 
-    print("\nTabela de Chegadas:")
-    print(f'{"Horário":<10} {"Código":<10} {"Portão":<10} {'Origem':<15} {"Status":<10}')
-    print("=" * 60)
-    atual = self.cabeca
-    while atual:
-        # Imprimir informações de cada voo por linha da tabela das CHEGADAS
-        if atual.chegando:
-            print(f'{atual.horario:<10} {atual.codigo:<10} {atual.portao:<10} {atual.destino:<15} {atual.status:<10}')
-        atual = atual.prox
+    def travessia(self):
+        if self.tamanho == 0:
+            print('Fila vazia! Não há voos para serem percorridos!')
+            return
+        # Imprima o cabeçalho da tabela das PARTIDAS 
+        print("Tabela de PARTIDAS")
+        print(f'{"Horário":<10} {"Código":<10} {"Portão":<10} {"Destino":<15} {"Status":<10}')
+        print("=" * 60)
+        atual = self.cabeca
+        # Imprimir informações de cada voo por linha da tabela das PARTIDAS
+        while atual:
+            if not atual.chegando: 
+                print(f'{atual.horario:<10} {atual.codigo:<10} {atual.portao:<10} {atual.destino:<15} {atual.status:<10}')
+            atual = atual.prox
+        print("\n" + "=" * 60)
+        # Imprimir o cabeçalho da tabela das CHEGADAS 
+        print("\nTabela de Chegadas:")
+        print(f'{"Horário":<10} {"Código":<10} {"Portão":<10} {"Origem":<15} {"Status":<10}')
+        print("=" * 60)
+        atual = self.cabeca
+        while atual:
+            # Imprimir informações de cada voo por linha da tabela das CHEGADAS
+            if atual.chegando:
+                print(f'{atual.horario:<10} {atual.codigo:<10} {atual.portao:<10} {atual.destino:<15} {atual.status:<10}')
+            atual = atual.prox
 # Imprimir uma tabela que satisfaça as informações do voo nessa ordem:
 # Horário
 # Código do voo
