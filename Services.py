f0165435ea1666863e7dfa6d836d13a9bb2410d5
TauaFrade <124211685+TauaFrade@users.noreply.github.com>
2023-12-02 21:04:35 -0300

    Update Services.py

--- Services.py
+++ Services.py
@@ -4,22 +4,28 @@
 # 0.1: INPLEMENTAÇÃO BASICA DE NO E LISTA DUPLAMENTE ENCADEADA
 
 class voo:
-    def __init__(self, horario, destino, chegando, codigo):
+    def __init__(self, horario,  codigo, portao, destino=None):
         #Informações do voo
         self.horario = horario
         self.status = 'Previto'
         self.destino = destino
-        self.chegando = chegando
         self.codigo = codigo
+        self.portao = portao
         #Ponteiros
         self.prox = None
         self.ulti = None
+        #modulo do horario
+        horas, minutos = self.horario.split(':')
+        horas = int(horas)
+        minutos = int(minutos)
+        self.mod_horario = (horas*60) + minutos
 
 class fila_de_voos:
-    def __init__(self):
+    def __init__(self, tipo):
         self.cabeca = None
         self.cauda = None
         self.tamanho = 0
+        self.tipo = tipo
 
     def remocao(self, codigo): # Remover um nó da fila 
         if self.tamanho == 0:
@@ -53,17 +59,15 @@ class fila_de_voos:
 
 # Ordenação
     def insertion_sort(self, fila):
-        for _ in range(self.tamanho):
-            noh_ref = self.cabeca
-            noh_comparador = self.cabeca.prox
-            ancora = noh_comparador.prox
-            while self.noh_ref.ulti and noh_ref.horario > noh_comparador.horario
-                #atualização dos ponteiros
-                noh_comparador.ulti = noh_ref.ulti
-                noh_ref.ulti = noh_comparador
-                noh_ref.prox = noh_comparador.prox
-                noh_comparador.prox = noh_ref
-                # atualização da refetencia
-                noh_ref = nof_comparador.ulti
-            # atualização do elemento a ser ordenado
-            noh_comparador = ancora 
+        noh_ref = self.cauda.ulti
+        noh_comparador = self.cauda
+        while self.noh_ref.ulti and noh_ref.horario > noh_comparador.horario:
+            if noh_comparador.codigo == self.cauda.codigo:
+                self.cauda = noh_ref
+            #atualização dos ponteiros
+            noh_comparador.ulti = noh_ref.ulti
+            noh_ref.ulti = noh_comparador
+            noh_ref.prox = noh_comparador.prox
+            noh_comparador.prox = noh_ref
+            # atualização da refetencia
+            noh_ref = noh_comparador.ulti
