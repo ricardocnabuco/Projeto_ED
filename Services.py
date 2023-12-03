2e7e67bed8eb24a6f2f754d3ceb46f7962233006
TauaFrade <124211685+TauaFrade@users.noreply.github.com>
2023-12-02 23:36:10 -0300

    Update Services.py

--- Services.py
+++ Services.py
@@ -4,11 +4,11 @@
 # 0.1: INPLEMENTAÇÃO BASICA DE NO E LISTA DUPLAMENTE ENCADEADA
 
 class voo:
-    def __init__(self, horario,  codigo, portao, destino=None):
+    def __init__(self, horario,  codigo, portao, destino_origem):
         #Informações do voo
         self.horario = horario
         self.status = 'Previto'
-        self.destino = destino
+        self.destino_origem = destino_origem
         self.codigo = codigo
         self.portao = portao
         #Ponteiros
@@ -21,17 +21,16 @@ class voo:
         self.mod_horario = (horas*60) + minutos
 
 class fila_de_voos:
-    def __init__(self, tipo):
+    def __init__(self):
         self.cabeca = None
         self.cauda = None
         self.tamanho = 0
-        self.tipo = tipo
 
 # Ordenação
-    def insertion_sort(self, fila):
+    def insertion_sort(self):
         noh_ref = self.cauda.anterior
         noh_comparador = self.cauda
-        while self.noh_ref.anterior and noh_ref.horario > noh_comparador.horario:
+        while noh_ref.anterior and noh_ref.horario > noh_comparador.horario:
             if noh_comparador.codigo == self.cauda.codigo:
                 self.cauda = noh_ref
             #atualização dos ponteiros
@@ -43,7 +42,7 @@ class fila_de_voos:
             noh_ref = noh_comparador.anterior
 
 # Remoção
-    def remocao(self, codigo): # Remover um nó da fila 
+    def remocao(self): # Remover um nó da fila 
         if self.tamanho == 0:
             print("Fila vazia! Não há voos para remover!") # Se tiver fila vazia
             return 
@@ -59,9 +58,9 @@ class fila_de_voos:
             print(f'Voo com código {codigo} removido!')
 
     # Insercao
-    def insercao(self, horario,  codigo, portao, destino=None): # adicionar um no
+    def insercao(self, horario,  codigo, portao, destino_origem): # adicionar um no
         self.tamanho += 1
-        novo_voo = voo(horario,  codigo, portao, destino=None)
+        novo_voo = voo(horario,  codigo, portao, destino_origem)
         if not self.cabeca:
             self.cabeca = novo_voo
             self.cauda = novo_voo
@@ -70,35 +69,37 @@ class fila_de_voos:
         novo_voo.anterior = self.cauda
         self.cauda = novo_voo
 
+
     #Busca
     # Como dois voos podem compartilhar o mesmo horario e essa variavel é a forma que eles são ordenados se faz necessario um
     # segundo parametro para a busca
-    def busca_binaria(self, codigo, horario, atual= self.cabeca, passos=(self.tamanho // 2)):
-        if atual == None:
+    def busca_binaria(self, codigo, horario, atual= -1, passos=-1):
+        aux_horas, aux_minutos = horario.split(':')
+        aux_horas = int(aux_horas)
+        aux_minutos = int(aux_minutos)
+        h_aux = (aux_horas*60) + aux_minutos
+        if atual == -1 and passos == -1:
+            atual = self.cabeca
+            passos = self.tamanho // 2
+        elif atual == None:
             print('Voo não encontrado, verifique se não houve nenhum erro de digitação')
             return
+        elif passos == 0:
+            passos += 1
         for _ in range(passos):
             atual = atual.prox
-        if atual.mod_horario == modulo_horario(horario):
+        if atual.mod_horario == h_aux:
             if atual.codigo == codigo: #Verifica se o voo encontrado não é outro com o mesmo horario
                 print(f'Voo {codigo} encontrado.')
                 print(f'Status: {atual.status}')
                 print(f'Horario: {atual.horario}')
                 print(f'Portão: {atual.portao}')
-                if atual.destino:
-                    print(f'Destino: {atual.destino}')
+                print(f'destino/origem: {atual.destino_origem}')
                 return
             else: # Caso seja verifica os proximos voos, pois pela forma de ordenação é onde estarão
-                self.busca_biniaria(codigo, horario, atual, 1)
-        elif atual.mod_horario >  modulo_horario(horario):
-            self.busca_binaria(codigo, horario, atual, passos//2)
-        else:
+                self.busca_binaria(codigo, horario, atual, 1)
+        elif atual.mod_horario >  h_aux:
             self.busca_binaria(codigo, horario, self.cabeca, passos//2)
+        else:
+            self.busca_binaria(codigo, horario, atual, passos//2)
 
-
-
-def modulo_horario(horario):
-    horas, minutos = horario.split(':')
-    horas = int(horas)
-    minutos = int(minutos)
-    return (horas*60) + minutos
